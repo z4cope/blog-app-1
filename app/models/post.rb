0b9c2595db2796fe9324ee653f34a7e810e32d92
belalgamal46 <belalgamal00@gmail.com>
2023-03-04 12:56:26 +0200

    Add unit test for like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validate :title, presence: true, length: { maximum: 250 }
-  validate :comments_counter, numericality: { only_integer: true, greater_than_or_equal: 0 }
-  validate :likes_counter, numericality: { only_integer: true, greater_than_or_equal: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
   def recent_comments
